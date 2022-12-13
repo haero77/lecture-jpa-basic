03db87baeec8ce628737ce2028dce4d45bde3898
Seonho Kim <sunhowold@naver.com>
2022-12-13 22:19:45 +0900

    엔티티 매핑 - 객체와 테이블 매핑, 데이터베이스 스키마 자동 생성

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -2,15 +2,34 @@ package hellojpa;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
 public class JpaMain {
 
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
         EntityManager em = emf.createEntityManager();
 
-        em.close();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            // 비영속
+            Member member = new Member();
+            member.setId(1L);
+            member.setName("HelloA");
+
+            // 영속
+            em.persist(member);
+
+            tx.commit(); // DB Query
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
         emf.close();
     }
 }
