0a09839a0cd40cd5b11a90a14ca4d3843231f5bc
Seonho Kim <sunhowold@naver.com>
2022-12-14 17:24:22 +0900

    실전 예제 1 - 데이터 중심 설계의 문제점 (도메인)

--- src/main/java/hellojpa/_03_relation_mapping/Team.java
+++ src/main/java/hellojpa/_03_relation_mapping/Team.java
@@ -1,9 +1,8 @@
 package hellojpa._03_relation_mapping;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Team {
@@ -14,6 +13,13 @@ public class Team {
     private Long id;
 
     private String name;
+
+    // 양방향 매핑 Team -> Member
+    // @OneToMany: 하나의 Team에 여러 Member
+    // mappedBy: Member클래스의 필드 'team' 과 매핑되어있다.
+    // 주인은 Member클래스의 'team'
+    @OneToMany(mappedBy = "team")
+    private List<Member> members = new ArrayList<>();
 
     public Long getId() {
         return id;
@@ -29,5 +35,9 @@ public class Team {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public List<Member> getMembers() {
+        return members;
     }
 }
